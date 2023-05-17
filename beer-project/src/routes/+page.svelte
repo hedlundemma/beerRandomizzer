64efb5d03e2356d0301a326aee840e1936b1065d
hedlundemma <112821036+hedlundemma@users.noreply.github.com>
2023-05-17 14:56:28 +0200

    trying out api-fetch

---
+++ beer-project/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { createQuery } from '@tanstack/svelte-query'
+  
+    // This data is cached by prefetchQuery in +page.ts so no fetch actually happens here
+    const query = createQuery({
+      queryKey: ['beers'],
+      queryFn: async () => (await fetch('/https://api.punkapi.com/v2/beers')).json()
+    })
+  </script>

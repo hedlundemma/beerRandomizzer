768c6b4e9f0eb7b973206a36c7ce42aa38a766cb
gustavenoksson <gustav.enoksson@live.se>
2023-05-23 13:09:49 +0200

    fixed typo in type Beer "food pariing""

--- beer-project/src/types/types.ts
+++ beer-project/src/types/types.ts
@@ -1,10 +1,9 @@
 // Skapa på mina vänner!
 
 export type Beer = {
-    food_pairing: any;
     id: number,
     name: string,
     description: string,
-    food_pariing: string,
+    food_pairing: string;
     image_url?: string
     }

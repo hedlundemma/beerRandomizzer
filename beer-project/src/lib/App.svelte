eb6c4fe2f121758ce2b467c92d8e3abe84acc1c6
filjon2001 <filip.jonasson@hotmail.com>
2023-05-27 14:45:28 +0200

    Adding Home.svelte as a start page with a button leading to BeerPage.svelte, making App.svelte a sort of routing page

--- beer-project/src/lib/App.svelte
+++ beer-project/src/lib/App.svelte
@@ -1,31 +1,30 @@
+<!-- App.svelte -->
 <script lang="ts">
-  import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
-  
-  import Layout from './Layout.svelte';
-  import Button from './Button.svelte';
-  import Input from './Input.svelte';
-  import SavedBeers from './SavedBeers.svelte';
-  import type { Beer } from '../types/types';
-  import '../app.css';
- 
- 
+  import { onMount } from 'svelte';
+  import Home from './Home.svelte';
+  import BeerPage from './BeerPage.svelte';
 
-  const queryClient = new QueryClient();
+  let currentPage = null;
+  let showElements = true;
 
-async function fetchBeer(): Promise<Beer[]> {
-  const data = await queryClient.fetchQuery(['beers']);
-  return data as Beer[];
-  
-}
+  onMount(() => {
+    // Set the initial page/component
+    currentPage = Home;
+  });
+
+  function goToBeerPage() {
+    currentPage = BeerPage;
+    showElements = false; // Hide the elements
+  }
 </script>
-<Layout>
-<QueryClientProvider client={queryClient}>
-  <section class = "beer-section">
-  <Button on:click={fetchBeer}></Button>
-  <Input/>
-  </section>
-  <SavedBeers/>
-</QueryClientProvider>
 
+<div>
+  {#if showElements}
+    <h1 style="color: black;">Welcome to beer heaven</h1>
+    <button on:click={goToBeerPage}>Get me a random beer</button>
+  {/if}
+</div>
 
-</Layout>
+{#if currentPage}
+  <svelte:component this={currentPage} />
+{/if}
---
+++ beer-project/src/lib/Home.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let name = 'Home';
+    export let goToBeerPage;
+    import Button from "./Button.svelte";
+    import App from "./App.svelte";
+</script>
